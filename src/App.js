0f5b4d9c1de71d95e9cda724ab0ace6e8c8138ca
deepa <deepamahesh0794@gmail.com>
2023-11-09 15:09:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [emails, setEmails] = useState('');
+  const [subject, setSubject] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSendEmails = async (e) => {
+   e.preventDefault()
+   console.log(e)
+    try {
+      const emailArray = emails.split('\n').map((email) => email.trim());
+      console.log(emailArray)
+      await axios.post('http://localhost:4000/sendEmail', {
+       recipients: emailArray,
+        subject,
+        text,
+      });
+      alert('Emails sent successfully.');
+    } catch (error) {
+      console.error(error);
+      alert('Error sending emails.');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form className='mail' onSubmit={(e)=>{handleSendEmails(e)}
+
+      }>
+       
+      <h1 className='bulk'>Bulk Mail Sender</h1>
+
+        <div className='to'>
+          <label>To:</label>
+      <textarea className='en'
+        placeholder="Enter email addresses "
+        value={emails}
+        onChange={(e) => setEmails(e.target.value)}
+      />
+      </div>
+      <div className='Subject'>
+      <label>Subject:</label>
+      <input className='sub'
+        type="text"
+        placeholder="Subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+      </div>
+     
+      <textarea className='text'
+        placeholder=" text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+            <input type='submit' class="custom-button"/>
+
+      </form>
+
     </div>
   );
 }
